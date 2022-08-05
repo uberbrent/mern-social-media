8d253320bc306b0c3e4eb8268215ccadd999b661
Brent Johnson <bjohnson@bjohnson.local.dct.com>
2022-08-05 16:01:26 -0500

    initial commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limig: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://uberbrent:Circa03@cluster0.edtoxwt.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('bufferCommands', false)
