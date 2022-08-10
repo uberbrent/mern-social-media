466bd5df769200115b4a6a3f37adc8f15e8c49e5
Brent M Johnson <70169219+uberbrent@users.noreply.github.com>
2022-08-10 13:57:20 -0500

    Update index.js
    updating server connection

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js'
 
@@ -13,10 +14,9 @@ app.use(bodyParser.urlencoded({ limig: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
-const CONNECTION_URL = "mongodb+srv://uberbrent:Circa03@cluster0.edtoxwt.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 4000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
