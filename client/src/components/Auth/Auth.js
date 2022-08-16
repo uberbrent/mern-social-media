2a3f97fd4a053f9e4d87940165e83bc8e927befd
uberbrent <bmjohns1337@gmail.com>
2022-08-16 16:38:26 -0500

    finished front end forms, styles and navigation on login no functionality

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -7,9 +7,8 @@ import useStyles from './styles';
 
 const Auth = () => {
     const [showPassword, setShowPassword] = useState(false);
+    const [isSignup, setIsSignup] = useState(false);
     const classes = useStyles();
-
-    const isSignup = false;
 
     const handleSubmit = () => {
 
@@ -19,9 +18,12 @@ const Auth = () => {
 
     }
 
-    const handleShowPassword = () => {
-        
+    const switchMode = () => {
+        setIsSignup((prevIsSignup) => !prevIsSignup);
+        handleShowPassword(false);
     }
+
+    const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword )
 
     return (
         <Container component="main" maxWidth="xs">
@@ -36,12 +38,23 @@ const Auth = () => {
                             isSignup && (
                                 <>
                                     <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
-                                    <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
+                                    <Input name="lastName" label="Last Name" handleChange={handleChange} autoFocus half />
                                 </>
                             )
                         }
                         <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
                         <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
+                        { isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" />}
+                    </Grid>
+                    <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+                        {isSignup ? 'Sign Up' : 'Sign In'}
+                    </Button>
+                    <Grid container justify="flex-end">
+                        <Grid item>
+                            <Button onClick={switchMode}>
+                                {isSignup ? 'Already have an account? Sign In' : 'Sign Up'}
+                            </Button>
+                        </Grid>
                     </Grid>
                 </form>
             </Paper>
@@ -49,4 +62,4 @@ const Auth = () => {
     )
 }
 
-export default Auth
+export default Auth;
